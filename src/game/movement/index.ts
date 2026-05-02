c52e533b11d9c2e67b80d54520223b0a79975be1
Wei Sing Ta <james.mma@gmail.com>
2026-05-02 23:44:58 +0000

    chore: add barrel exports for units and movement modules

---
+++ src/game/movement/index.ts
@@ -0,0 +1,1 @@
+export { computeMoveRange } from './MoveRange';
---
+++ src/game/units/index.ts
@@ -0,0 +1,4 @@
+export { Unit, Faction, UnitClass } from './Unit';
+export type { Faction as FactionType, UnitClass as UnitClassType } from './Unit';
+export { createStats } from './Stats';
+export type { UnitStats, UnitStatsInput } from './Stats';
