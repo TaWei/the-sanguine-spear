31d479ab773386be9f4a0f374d19db7ede1220a5
Wei Sing Ta <james.mma@gmail.com>
2026-05-02 23:51:03 +0000

    chore: add barrel export for state module

---
+++ src/game/state/index.ts
@@ -0,0 +1,6 @@
+export { TurnManager, GamePhase } from './TurnManager';
+export type { GamePhase as GamePhaseType } from './TurnManager';
+export { ActionQueue, ActionType } from './ActionQueue';
+export type { Action, ActionType as ActionTypeType } from './ActionQueue';
+export { UnitState, UNIT_STATE } from './UnitState';
+export type { UnitStateType } from './UnitState';
