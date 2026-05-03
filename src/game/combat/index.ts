3f4cd16bdba018ae7fba1558319f9b939aafb85b
Wei Sing Ta <james.mma@gmail.com>
2026-05-03 00:01:22 +0000

    feat: add combat module barrel exports

---
+++ src/game/combat/index.ts
@@ -0,0 +1,6 @@
+export { WEAPON_DB, WeaponType, getWeaponTriangleMod } from './Weapons';
+export type { WeaponData, TriangleMod } from './Weapons';
+export { calcHitRate, calcAvoid, calcDisplayHit, calcCritRate, calcCritAvoid, calcDamage, rollTrueHit, rollCrit } from './Formulas';
+export { CombatEngine } from './Engine';
+export type { CombatLogEntry, CombatResult } from './Engine';
+export { computeAttackRange } from './AttackRange';
