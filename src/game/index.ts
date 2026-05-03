1e9b2c612400e7015d6348e095a399e47d5c3638
Wei Sing Ta <james.mma@gmail.com>
2026-05-03 00:19:59 +0000

    feat: add game engine barrel export

---
+++ src/game/index.ts
@@ -0,0 +1,7 @@
+export { GameEngine } from './GameEngine';
+export * from './map';
+export * from './units';
+export * from './movement';
+export * from './combat';
+export * from './ai';
+export * from './state';
