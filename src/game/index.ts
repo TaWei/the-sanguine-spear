a516b6b0e307bd50453336626b807b3460094b13
Wei Sing Ta <james.mma@gmail.com>
2026-05-04 16:20:19 +0000

    chore: wire up staves module exports

--- src/game/index.ts
+++ src/game/index.ts
@@ -3,6 +3,7 @@ export * from './map';
 export * from './units';
 export * from './movement';
 export * from './combat';
+export * from './staves';
 export * from './ai';
 export * from './state';
 export * from './progression';
---
+++ src/game/staves/index.ts
@@ -0,0 +1,6 @@
+export { STAFF_DB } from './Staves';
+export type { StaffData } from './Staves';
+export { computeStaffRange } from './StaffRange';
+export { getHealTargets } from './getHealTargets';
+export { StaffEngine } from './StaffEngine';
+export type { StaffResult } from './StaffEngine';
