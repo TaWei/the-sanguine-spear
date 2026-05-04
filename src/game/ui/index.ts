31483233d30953abfab47807f5a09c40b17de987
Wei Sing Ta <james.mma@gmail.com>
2026-05-04 15:33:08 +0000

    chore(ui): export LevelUpDisplay from barrel

--- src/game/ui/index.ts
+++ src/game/ui/index.ts
@@ -2,3 +2,4 @@ export { BattleMenu, MenuState, MenuAction } from './BattleMenu';
 export { ItemMenu, ItemMenuState, Item, ItemUseResult } from './ItemMenu';
 export { BattleDisplayState, BattlePhase } from './BattleDisplayState';
 export { StatusWindow, StatusDisplay } from './StatusWindow';
+export { LevelUpDisplay, LEVEL_UP_PHASE } from './LevelUpDisplay';
